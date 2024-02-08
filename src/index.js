83e6b621e67fd48c218da1e1e862d647cd7ce969
Aniket Thakre <aniketthakre2005@gmail.com>
2024-02-08 22:35:40 +0530

    update1
    update 1

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Page3 from './Components/page3/page3';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Page3 />
+    <Page1 />
   </React.StrictMode>
 );
 
